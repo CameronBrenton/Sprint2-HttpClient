9a0955a8ae297fa46eefa47c6fd53a8cae4ea678
CameronBrenton <Cameron.Brenton@keyin.com>
2021-12-10 11:10:53 -0330

    First Commit

---
+++ src/main/java/com/keyin/HttpClient/HttpClientApplication.java
@@ -0,0 +1,13 @@
+package com.keyin.HttpClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keyin/HttpClient/HttpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyin.HttpClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
